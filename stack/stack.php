4b18e3110eb0560ebbebf62e66c4824c483b8ee9
Alexandre Osovski <alexandreosovski1@gmail.com>
2024-05-21 21:56:00 -0300

    first commit

---
+++ stack/stack.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace stack;
+
+#LIFO STRUCTURE
+#Handmade
+class stack
+{
+    private int $size;
+
+    private array $structure = [];
+
+    const MAX_WIDTH = 592734987549723473;
+
+    public function __construct()
+    {
+        $this->size = 0;
+        $this->structure = [];
+    }
+
+    public function __destruct()
+    {
+        unset($this->structure);
+    }
+
+    public function push($item): void
+    {
+        if ($this->size > self::MAX_WIDTH) {
+            echo 'the structure ins full';
+        } else {
+            $this->structure[$this->size] = $item;
+            $this->size++;
+        }
+    }
+    public function pop(): void
+    {
+        if ($this->empty()) {
+            echo 'This structure is empty';
+        } else {
+            unset($this->structure[$this->size - 1]);
+            $this->size--;
+        }
+    }
+
+    public function empty(): bool
+    {
+        return empty($this->structure);
+    }
+
+    public function getSizeStructure(): array
+    {
+        $array = [
+            'size' => count($this->structure),
+            'memory_used' => memory_get_usage() / 1024 / 1024
+        ];
+        return $array;
+    }
+}
+
+$stack = new stack();
+
+for ($i = 0; $i < 10; $i++) {
+    $stack->push($i);
+}
+
+$stack->pop();
+
+// var_dump($stack->getSizeStructure());
+
+#using function php
+
+$structure = [];
+array_push($structure, 'item one');
+
+var_dump($structure);
